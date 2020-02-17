4a96652b29e12aa8c23af7f29f7fd0fb9340f126
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-17 18:01:08 -0400

    reverstring by myself

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/**
+ * First Approach to the solution
+ * Create an array with the size of the string
+ * Do a for front to back and assign each letter to the array
+ * @param {String} str String to reverse
+ * @return {String} Return and string reversed 
+ */
+function reverse(str) {
+    var tmp = new Array(str.length);
+
+    for (let i = str.length - 1, j = 0; i >= 0; i--, j++) {
+        tmp[j] = str[i];
+    }
+
+    return tmp.join("");
+}
 
 module.exports = reverse;
