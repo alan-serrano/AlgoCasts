ff2f38013afe7886a8a1391ab3cae288dcb8430e
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-19 17:43:08 -0400

    FizzBuzz Exercise first approach

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,16 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for( let i = 1; i <= n; i++) {
+        let tmp = '';
+
+        if( i%3 === 0 )     tmp += 'fizz'; 
+        if ( i%5 === 0 )    tmp += 'buzz';
+        if( !tmp )          tmp = i;
+
+        console.log(tmp);
+    }
+}
 
 module.exports = fizzBuzz;
