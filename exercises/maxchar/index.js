a85489617b58a1d9b55f1f919f9165334d6f4ecb
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-19 17:04:18 -0400

    First Approach maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    //if the string is empty return null
+    if(str.length === 0) return null;
+    
+    // Initialize the max value
+    var letters = {
+        max: str[0]
+    };
+
+    for (const ch of str) {
+        if(!letters.hasOwnProperty(ch)) {
+            letters[ch] = 1;
+
+        } else {
+            letters[ch]++;
+        }
+
+        // Saving the max value in the object Letters
+        if( letters[ch] > letters[letters.max] ) {
+            letters.max = ch;
+        }
+    }
+
+    return letters.max;
+}
 
 module.exports = maxChar;
