6580bfcf658db593116510c1fcb151f375653730
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-19 17:56:26 -0400

    Solution of Udemy Video

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -24,4 +24,21 @@ function fizzBuzz(n) {
     }
 }
 
-module.exports = fizzBuzz;
+/* Solution of the video */
+function fizzBuzz1(n) {
+    for( let i = 1; i <= n; i++) {
+        //Is the number a multiple of 3 and 5?
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            // Is the number a multiple of 3?
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+module.exports = fizzBuzz1;
