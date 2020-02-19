7e8b038fc0dd8ce74a4de3eb0320ddc01e9ba435
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-19 18:45:13 -0400

    chunk exercise first approach

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    var tmp = []; // Array Helper
+    var counter = 0; // Pointer of subArrays
+
+    // Initialize all the subarrays
+    for(let i = 1; i <= Math.ceil(array.length / size); i++) {
+        tmp.push([]);
+    }
+
+    // Each multiple of size, mean another subArray
+    array.forEach((el, i) => {
+        if(i % size === 0 && i !== 0) {
+            counter++;
+        }
+
+        tmp[counter].push(el);
+    });
+
+    return tmp;
+}
 
 module.exports = chunk;
