1d57927791d263da661540e8925a1c665b7f14c3
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-22 00:24:03 -0400

    vowels, my solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,28 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    // Create a map to store the vowels, the values are not important
+    const vowels = {
+        a: null,
+        e: null,
+        i: null,
+        o: null,
+        u: null
+    }
+
+    // Create a counter of the vowels
+    let counter = 0;
+
+    // Do a cycle for each character of the string
+    // If the character is a vowel, increment the counter
+
+    for (const ch of str.toLowerCase()) {
+        if( vowels.hasOwnProperty(ch) ) counter++;
+    }
+
+    // return the counter
+    return counter;
+}
 
 module.exports = vowels;
