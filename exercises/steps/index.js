4cfd1df7c2c874bb3cc81a0b1b7555b0fc307c3c
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-21 20:22:36 -0400

    steps, Solutions of video

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -35,4 +35,43 @@ function steps(n) {
     }
 }
 
-module.exports = steps;
+
+/* Solution 1 of the video */
+function steps1(n) {
+    for( let row = 0; row < n; row++ ) {
+        let stair = '';
+
+        for (let column = 0; column < n; column++) {
+            if( column <= row ) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(stair);
+    }
+}
+
+/* Solution 2 with recursion */
+
+function steps2(n, row = 0, stair = '') {
+    if( n === row ) {
+        return;
+    }
+
+    if(n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if(stair.length <= row ) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+}
+
+module.exports = steps2;
