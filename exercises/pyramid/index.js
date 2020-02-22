54c5bf8c0afe584a62115e5eae72a271c71f3ce5
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-21 21:46:10 -0400

    pyramid my solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    var spaces = 2*n - 1;
+    var middle = Math.floor( spaces/2 );
+    for( let row = 0; row < n; row++) {
+        let stair = '';
+        let start = middle - row;
+        let end = middle + row;
 
-module.exports = pyramid;
+        for( let column = 0; column < spaces; column++ ) {
+            debugger;
+            if(column >= start && column <= end) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(stair);
+    }
+}
+
+function pyramidRecursively(n, row = 0, stair = '') {
+    let spaces = 2*n - 1;
+    let middle = Math.floor( spaces/2 );
+    let start = middle - row;
+    let end = middle + row;
+    
+    if(n === row) return;
+
+    if(stair.length === spaces) {
+        console.log(stair);
+        pyramidRecursively(n, row + 1);
+        return;
+    }
+
+    if( stair.length >= start && stair.length <= end ) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    pyramidRecursively(n, row, stair);
+}
+
+module.exports = pyramidRecursively;
