4e9fe85f35ad2458f00c01d76da7e051db694e65
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-26 16:00:37 -0400

    Fibbonacci My solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,33 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Iterative solution
+function fib(n) {
+    if( n === 0 ) {
+        return 0;
+    }
+    
+    if ( n === 1 ) {
+        return 1;
+    }
+    
+    const series = [0, 1];
 
-module.exports = fib;
+    for (let i = 2; i <= n; i++) {
+        series.push( series[i-2] + series[i-1] );
+    }
+
+    return series[n];
+}
+
+//Recursive solution
+
+function fibRecursive(n) {
+    // Base case
+    if (n === 0) return 0;
+    if ( n === 1 ) return 1;
+
+    return fib(n-2) + fib(n-1);
+}
+
+module.exports = fibRecursive;
